5ec16ec52f7d4eadd330ead50309f168bb1e0179
unknown <shakir.evan.3070@Fec.edu.bd>
2025-09-21 19:26:21 +0600

    Navbar part done

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react' // <-- Add this line
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Components/Root';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:'/home',
+        element:<Home></Home>
+      },
+      {
+         path:'/login',
+         element:<Login></Login>
+      },
+      { path:'/register',
+        element:<Register></Register>
+      }
+    ]
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
