60f119fb1b15afc387b365bb42202853ea7da417
unknown <shakir.evan.3070@Fec.edu.bd>
2025-09-23 15:58:20 +0600

    register part

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -43,7 +43,7 @@ const Register = () => {
     <div className="">
       <div className="mx-auto md:w-1/2">
         <h2 className="text-3xl py-2 px-4 mb-5">Please Register</h2>
-        <form onSubmit={handleRegister} className="border p-5 rounded">
+        <form onSubmit={handleRegister} className="p-5 rounded">
           <input
             className="mb-4 w-3/4 border py-2 px-4"
             type="email"
@@ -53,25 +53,25 @@ const Register = () => {
             required
           />
           <br />
-          <input
-            className="mb-4 w-3/4 border py-2 px-4"
-            type={showPassword ? "text" : "password"}
-            name="password"
-            id=""
-            placeholder="Password"
-            required
-          />
-          <span onClick={() => setShowPassword(!showPassword)}>
-            {
-              showPassword ? <IoEyeOffSharp/> : <FaEye/>
-            }
-          </span>
-          <br />
-          <input
-            className="btn btn-secondary mb-4 w-3/4"
-            type="submit"
-            value="Register"
-          />
+          <div className="relative">
+            <input
+              className="w-3/4 border py-2 px-4 mb-4"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id=""
+              placeholder="Password"
+              required
+            />
+            <span className="absolute top-2 text-2xl ml-1" onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <IoEyeOffSharp /> : <FaEye />}
+            </span>
+            <br />
+            <input
+              className="btn btn-secondary mb-4 w-3/4"
+              type="submit"
+              value="Register"
+            />
+          </div>
         </form>
         {registerError && <p className="text-red-600">{registerError}</p>}
         {Success && <p className="text-green-600">{Success}</p>}
