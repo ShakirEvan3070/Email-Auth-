4a10800957d9a30ccb8cf38e0af018054d1c172f
unknown <shakir.evan.3070@Fec.edu.bd>
2025-09-23 12:00:15 +0600

    email password auth part done

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,19 +1,37 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "./Firebase.config";
+import { useState } from "react";
 const Register = () => {
+
+  const[registerError , setRegisterError]= useState();
+  const[Success , setSuccess] = useState('');
 
     const handleRegister = e =>{
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email,password);
+
+        if(password.length < 6){
+          setRegisterError('Password should be 6 characters or more');
+          return;
+        }
+
+        // reset error 
+        setRegisterError('');
+        setSuccess('');
+
+
+        // create User 
         createUserWithEmailAndPassword(auth, email, password)
         .then(result =>{
             const user = result.user;
-            console.log(user);
-            e.target.reset();
+            // console.log(user);
+            // e.target.reset();
+            setSuccess('User Created Successfully');
         }) .catch(error =>{
-            console.error('error', error);
+          setRegisterError(error.message);      
+            // console.error('error', error);
         })
     }
 
@@ -22,12 +40,18 @@ const Register = () => {
       <div className="mx-auto md:w-1/2">
         <h2 className="text-3xl py-2 px-4 mb-5">Please Register</h2>
         <form onSubmit={handleRegister} className="border p-5 rounded">
-          <input className="mb-4 w-3/4 border py-2 px-4" type="email"  name="email" id="" placeholder="Email address"/>
+          <input className="mb-4 w-3/4 border py-2 px-4" type="email"  name="email" id="" placeholder="Email address" required/>
           <br />
-          <input className="mb-4 w-3/4 border py-2 px-4" type="password" name="password" id="" placeholder="Password"/>
+          <input className="mb-4 w-3/4 border py-2 px-4" type="password" name="password" id="" placeholder="Password" required/>
           <br />
           <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
         </form>
+        {
+          registerError && <p className="text-red-600">{registerError}</p>
+        }
+        {
+          Success && <p className="text-green-600">{Success}</p>
+        }
       </div>
     </div>
   );
